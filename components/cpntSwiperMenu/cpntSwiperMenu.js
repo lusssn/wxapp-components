f1e0c064760e9a3e7a04515ea9121570f90f36d0
lusssn <lusssn@126.com>
2018-01-03 15:07:59 +0800

    A 新增 可滑动菜单说明；U 优化 可滑动菜单滑条跳转方式

--- components/cpntSwiperMenu/cpntSwiperMenu.js
+++ components/cpntSwiperMenu/cpntSwiperMenu.js
@@ -2,49 +2,45 @@ Component({
   properties: {
     menuWidth: {
       type: Number, value: 160
+    },
+    threshold: {
+      type: Number, value: 40
     }
   },
   options: {
     multipleSlots: true // 在组件定义时的选项中启用多slot支持
   },
-  attached () {
-    this.setData({
-      scrollLeft: this.data.menuWidth
-    })
-  },
   methods: {
     onSwitchMenu () {
-      const scrollLeft = this.data.scrollLeft
       this.setData({
-        scrollLeft: scrollLeft ? 0 : this.data.menuWidth
+        switchLeft: this.data.menuWidth - (this.data.switchLeft || 0)
       })
     },
     onTouchStart (e) {
+      const touch = e.changedTouches[0] || e.touches[0]
       this.setData({
-        touchStartX: e.changedTouches[0].clientX
+        touchStartX: touch.clientX
       })
     },
     onTouchEnd (e) {
       const _this = this
-      const menuWidth = _this.data.menuWidth
       const margin = e.changedTouches[0].clientX - _this.data.touchStartX
-      const threshold = Math.floor(menuWidth / 3)
       // 向右滑动
-      if (margin > threshold) {
+      if (margin > _this.data.threshold) {
         _this.setData({
-          scrollLeft: 0
+          switchLeft: _this.data.menuWidth
         })
         return
       }
       // 向左滑动
-      if (margin < -threshold) {
+      if (margin < -(_this.data.threshold)) {
         _this.setData({
-          scrollLeft: menuWidth
+          switchLeft: 0
         })
         return
       }
       _this.setData({
-        scrollLeft: _this.data.scrollLeft
+        switchLeft: _this.data.switchLeft
       })
     }
   }
